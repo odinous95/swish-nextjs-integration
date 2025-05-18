75ab7d3afb4816992311aa7adfa7398cc64f89ae
Odin Thif <mjd.thif@icloud.com>
2025-05-18 15:19:47 +0200

    ssl cert config

--- src/services/swish/swish.ts
+++ src/services/swish/swish.ts
@@ -12,25 +12,16 @@ export interface SwishConfig {
 }
 
 export function getSwishConfig(): SwishConfig {
-  const certPath = path.resolve(
-    __dirname,
-    "../../../" + process.env.SWISH_CERT_PATH!
-  );
-  const keyPath = path.resolve(
-    __dirname,
-    "../../../" + process.env.SWISH_KEY_PATH!
-  );
-  const caPath = path.resolve(
-    __dirname,
-    "../../../" + process.env.SWISH_CA_PATH!
-  );
+  const cert = Buffer.from(process.env.SWISH_CERT_BASE64!, "base64");
+  const key = Buffer.from(process.env.SWISH_KEY_BASE64!, "base64");
+  const ca = Buffer.from(process.env.SWISH_CA_BASE64!, "base64");
 
   const agent = new https.Agent({
-    cert: fs.readFileSync(certPath),
-    key: fs.readFileSync(keyPath),
-    ca: fs.readFileSync(caPath),
+    cert,
+    key,
+    ca,
     passphrase: process.env.SWISH_PASSPHRASE,
-    // rejectUnauthorized: true, // must be true in production
+    // rejectUnauthorized: true,
   });
 
   return {
