865dcc59410595f0619b716bfb0f27e591664097
Odin Thif <mjd.thif@icloud.com>
2025-05-18 14:52:42 +0200

    changed ssl path

--- src/services/swish/swish.ts
+++ src/services/swish/swish.ts
@@ -12,9 +12,9 @@ export interface SwishConfig {
 }
 
 export function getSwishConfig(): SwishConfig {
-  const certPath = path.resolve(process.cwd(), process.env.SWISH_CERT_PATH!);
-  const keyPath = path.resolve(process.cwd(), process.env.SWISH_KEY_PATH!);
-  const caPath = path.resolve(process.cwd(), process.env.SWISH_CA_PATH!);
+  const certPath = path.resolve(process.env.SWISH_CERT_PATH!);
+  const keyPath = path.resolve(process.env.SWISH_KEY_PATH!);
+  const caPath = path.resolve(process.env.SWISH_CA_PATH!);
 
   const agent = new https.Agent({
     cert: fs.readFileSync(certPath),
