9becc95476700094905b98ccebbe6647a6fab1fb
Odin Thif <mjd.thif@icloud.com>
2025-05-29 17:12:05 +0200

    new checkout

--- src/app/api/swish/paymentrequests/[requestId]/route.ts
+++ src/app/api/swish/paymentrequests/[requestId]/route.ts
@@ -1,10 +1,52 @@
+// import { swishConfig, swishRequest } from "@/services/swish/swish";
+// import { NextRequest, NextResponse } from "next/server";
+
+// export async function GET(req: NextRequest) {
+//   try {
+//     const requestId = req.nextUrl.pathname.split("/").pop();
+//     console.log("Request ID:", requestId);
+
+//     if (!requestId) {
+//       return NextResponse.json(
+//         { error: "Missing requestId in path" },
+//         { status: 400 }
+//       );
+//     }
+//     const url = `${swishConfig.host}/api/v1/paymentrequests/${requestId}`;
+//     const resp = await swishRequest("GET", url);
+
+//     const data = resp.data as {
+//       id: string;
+//       paymentReference?: string;
+//       status: string;
+//     };
+
+//     return NextResponse.json({
+//       id: data.id,
+//       paymentReference: data.paymentReference || "",
+//       status: data.status,
+//     });
+//   } catch (error: any) {
+//     console.error(error);
+//     return NextResponse.json(
+//       {
+//         error: "Failed to fetch payment request",
+//         message: error.message || "Unexpected error",
+//       },
+//       { status: 500 }
+//     );
+//   }
+// }
+
 import { swishConfig, swishRequest } from "@/services/swish/swish";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { requestId: string } }
+) {
   try {
-    const requestId = req.nextUrl.pathname.split("/").pop();
-    console.log("Request ID:", requestId);
+    const { requestId } = params;
 
     if (!requestId) {
       return NextResponse.json(
@@ -12,6 +54,7 @@ export async function GET(req: NextRequest) {
         { status: 400 }
       );
     }
+
     const url = `${swishConfig.host}/api/v1/paymentrequests/${requestId}`;
     const resp = await swishRequest("GET", url);
 
