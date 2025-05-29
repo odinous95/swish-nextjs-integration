e8579f40b0d1e9ffbb84507e825089eee4de190f
Odin Thif <mjd.thif@icloud.com>
2025-05-29 21:43:13 +0200

    fixed swish cofing for switchiing between test and prod

--- src/services/swish/swish.ts
+++ src/services/swish/swish.ts
@@ -26,16 +26,16 @@ const prodConfig: SwishConfig = {
   passphrase: null,
 };
 
-const testConfig: SwishConfig = {
-  payeeAlias: "1234679304",
-  host: "https://mss.cpc.getswish.net/swish-cpcapi",
-  qrHost: "https://mpc.getswish.net/qrg-swish",
-  cert: fs.readFileSync(path.resolve(process.env.SWISH_CERT_TEST || "")),
-  key: fs.readFileSync(path.resolve(process.env.SWISH_KEY_TEST || "")),
-  ca: fs.readFileSync(path.resolve(process.env.SWISH_CA_TEST || "")),
+// const testConfig: SwishConfig = {
+//   payeeAlias: "1234679304",
+//   host: "https://mss.cpc.getswish.net/swish-cpcapi",
+//   qrHost: "https://mpc.getswish.net/qrg-swish",
+//   cert: fs.readFileSync(path.resolve(process.env.SWISH_CERT_TEST || "")),
+//   key: fs.readFileSync(path.resolve(process.env.SWISH_KEY_TEST || "")),
+//   ca: fs.readFileSync(path.resolve(process.env.SWISH_CA_TEST || "")),
 
-  passphrase: "swish",
-};
+//   passphrase: "swish",
+// };
 
 export async function swishRequest<T>(
   method: "POST" | "GET",
@@ -63,4 +63,5 @@ export async function swishRequest<T>(
   return response;
 }
 
-export const swishConfig = isProd ? prodConfig : testConfig;
+// export const swishConfig = isProd ? prodConfig : testConfig;
+export const swishConfig = prodConfig;
